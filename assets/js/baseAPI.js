8a64f937ba55dc538aa512744b576581bd241529
liuwusuile <18052019783@163.com>
2022-09-30 15:34:57 +0800

    完成了首页权限相关的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -11,5 +11,21 @@ $.ajaxPrefilter(function (options) {
   }
   options.url = 'http://big-event-vue-api-t.itheima.net' + options.url
   options.contentType = 'application/json'
-  options.data = format2Json(options.data)
+  options.data = options.data && format2Json(options.data)
+  // 如果请求路径里面有/my 这样的字符串 就需要添加headers
+  // indexOf startsWith endsWith  includes 包含 包括
+  // if (options.url.includes('my')) 
+  if (options.url.indexOf('/my/'!==-1)) {
+    // options.headers['Authorization'] = localStorage.getItem('big_news_token') || ''
+    options.headers={
+      'Authorization':localStorage.getItem('big_news_token') || ''
+    }
+  }
+  // 统一添加错误回调
+  options.error=function(err){
+    if (err.responseJSON?.code==1&&err.responseJSON?.message==='身份认证失败！') {
+      localStorage.clear()
+      location.href='./login.html'
+    }
+  }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,43 @@
-$(function(){
-  
+let layer = layui.layer
+$(function () {
+  getUserInfo()
+})
+
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // url: 'http://big-event-vue-api-t.itheima.net/my/userinfo',
+    // headers: {
+    //   Authorization: localStorage.getItem('big_news_token') || ''
+    // },
+    success: function (res) {
+      console.log(res)
+      if (res.code !== 0) return layer.msg(res.message)
+      renderAvatar(res)
+    },
+    // complete:function(){
+    // }
+
+  })
+}
+const renderAvatar = function (res) {
+  if (res.data.user_pic) {
+    $('.text-avater').hide()
+    $('.userinfo img').css('src', res.data.user_pic)
+  } else {
+    $('.userinfo img').hide()
+    const name = res.data.nickname || res.data.username
+    const chat = name.charAt(0).toUpperCase()
+    $('.text-avater').html(chat)
+  }
+  $('.userinfo .text').html(`欢迎&nbsp;&nbsp;${res.data.username}`)
+}
+$('#btnLogout').on('click',function(){
+  layer.confirm('你确定要退出吗?', {icon: 3, title:'提示'}, function(index){
+    localStorage.removeItem('big_news_token')
+    location.href='./login.html'
+    
+    layer.close(index);
+  });
 })
